828437c766bca7affec462b720e78b9eadf6e490
Manuel Zegarra <mzegarra@gmail.com>
2016-02-05 20:38:51 -0500

    Version 2

--- service-zuul/src/main/java/com/example/ZuulServiceApplication.java
+++ service-zuul/src/main/java/com/example/ZuulServiceApplication.java
@@ -3,10 +3,12 @@ package com.example;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
 
 
 @Configuration
@@ -14,8 +16,12 @@ import org.springframework.context.annotation.Configuration;
 @EnableAutoConfiguration(exclude = {ErrorMvcAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableZuulProxy
+@Controller
 public class ZuulServiceApplication {
    public static void main( String[] args ){
-      SpringApplication.run(ZuulServiceApplication.class, args);
+      //SpringApplication.run(ZuulServiceApplication.class, args);
+      
+      new SpringApplicationBuilder(ZuulServiceApplication.class).web(true).run(args);
+     
    }
 }
