0b2d482006d3d8dda645dd1b3cbcad94c4e1fed9
diego <diegoaguirre@hotmail.com>
2016-02-25 18:48:46 -0500

    cambios web

--- web_demo/src/main/java/com/example/controller/DocumentsController.java
+++ web_demo/src/main/java/com/example/controller/DocumentsController.java
@@ -1,5 +1,14 @@
 package com.example.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RefreshScope
+@EnableAutoConfiguration
+@RequestMapping("/documents")
 public class DocumentsController {
 
 }
--- web_demo/src/main/java/com/example/controller/StoreController.java
+++ web_demo/src/main/java/com/example/controller/StoreController.java
@@ -1,5 +1,20 @@
 package com.example.controller;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RefreshScope
+@EnableAutoConfiguration
+@RequestMapping("/store")
 public class StoreController {
 
+	
+	@RequestMapping(value = "/index", method = RequestMethod.GET)
+	public String index(){
+		return "store/index";
+	}
 }
