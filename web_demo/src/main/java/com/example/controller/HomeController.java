7f1ded3bb451e9e7e3d5fcb1ac08828a69388cd7
DiegoAguirre <DiegoAguirre@192.168.0.102>
2016-02-25 01:53:27 -0500

    cambios web

--- web_demo/src/main/java/com/example/controller/HomeController.java
+++ web_demo/src/main/java/com/example/controller/HomeController.java
@@ -3,28 +3,19 @@ package com.example.controller;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.example.model.Person;
 
 @Controller
 @RefreshScope
 @EnableAutoConfiguration
 @RequestMapping("/")
-public class PersonController {
+public class HomeController {
 
-	@RequestMapping(value="/",method=RequestMethod.GET)
-	public String person(Model model){
-		Person person = new Person();
-		person.setAge(2);
-		person.setFirstName("diego");
-		person.setLastName("aguirre");
-		model.addAttribute("person",person);
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String home() {
 		
 		return "home/index";
 	}
-	
-
 }
